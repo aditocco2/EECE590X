452514091a5a026c3f64701745639b4432ac45cf
speedycube64 <aditocco2@gmail.com>
2025-09-16 13:00:03 -0400

    Updated Q2 and put correct CSVs in Q3 through Q5

--- 11_Size_Optimization_Using_Karnaugh_Maps/2_read_terms_kmap/read_terms_kmap.py
+++ 11_Size_Optimization_Using_Karnaugh_Maps/2_read_terms_kmap/read_terms_kmap.py
@@ -16,11 +16,12 @@ groups = {
 count = 0
 
 for key in groups:
-    draw_kmap(key,f'kmap_{count}')
+    # draw_kmap(key,f'kmap_{count}')
+    # Already done
 
     question_text = f"<p>Determine the algebraic expression for the \
-            terms circled in the Karnaugh Map shown above.</p>"
-
+            terms circled in the Karnaugh Map shown above. Specify \
+            your answer using only lowercase letters with no parentheses.</p>"
 
     question = d2l.SAQuestion(question_text)
 
@@ -29,6 +30,7 @@ for key in groups:
     feedback = "+".join(groups[key])
 
     for i in range(len(groups[key])-1):
+
         regex_ans += f"\\s*({ans})(?!.*\\{i+1}[^'a-z])\\s*\\+"
     regex_ans += f"\\s*({ans})\\s*$"
     question.add_image(f"/imagepools/alivebeef/kmap_{count}.png")
