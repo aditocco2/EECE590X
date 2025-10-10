8d393b68aabe3c1afd2d2cc184babf770e584373
speedycube64 <aditocco2@gmail.com>
2025-10-09 22:44:34 -0400

    STILL working on fsm to tt conversion
    lalala im going insane

---
+++ 18_Finite_State_Machines_1/fsm_class_test.py
@@ -0,0 +1,10 @@
+import fsm_data
+
+fsm = fsm_data.FSMData("fsm_2.txt")
+
+print(f"state names: {fsm.state_names}, state bit combos: {fsm.state_bit_combos}")
+print(f"input names: {fsm.input_names}, input combos: {fsm.input_combos}")
+print(f"outputs: {fsm.output_names}")
+print(f"state data: {fsm.state_data}")
+print(f"state bits: {fsm.state_bit_names}, next_state_bits = {fsm.next_state_bit_names}")
+
---
+++ 18_Finite_State_Machines_1/fsm_data.py
@@ -0,0 +1,144 @@
+from TruthTableHTML.html_tt import html_tt
+from logic_eval import logic_eval
+import itertools
+import json
+import re
+
+class FSMData():
+
+    def __init__(self, filename, state_notation = "Q"):
+        with open(filename, "r") as f:
+            self.fsm_json = json.load(f)
+        self.get_inputs_from_json()
+        self.get_outputs_from_json()
+        self.get_states_from_json(state_notation)
+        self.organize_state_data()
+    
+    def organize_state_data(self):
+
+        # Makes a list of states with all their arcs and outputs
+
+        self.state_data = []
+
+        for current_state in self.state_names:
+
+            current_state_data = {"state": current_state, "outputs": {}, "arcs": []}
+
+            # Find the output text corresponding to the current state
+            for node in self.fsm_json["fsmNodes"]:
+                if node["stateName"] == current_state:
+                    text = node["outputText"]
+            # replace everything but alphanumeric and underscore with space
+            text = re.sub("[^0-9a-zA-Z_]+", " " , text)
+            output_words = text.replace("=", ",").split(",") #["F", "1", "G", "0"]
+
+            for output in self.output_names:
+                # If an output exists under the state name, copy it to the outputs dict
+                if output in output_words:
+                    output_value = output_words.index(output) + 1
+                    current_state_data["outputs"][output] = output_value
+                # Otherwise assume it's 0
+                else:
+                    current_state_data["outputs"][output] = "0"
+
+            # Since arcs anchor to node ID and not state name, we have to use the arc's
+            # start node ID to index into the list of states
+            # They are also stored in two separate lists: fsmArcs and fsmSelfArcs
+            
+            for arc in self.fsm_json["fsmArcs"]:
+                state_arc_leaves = self.fsm_json["fsmNodes"][arc["startNode"]]["stateName"]
+                state_arc_goes_to = self.fsm_json["fsmNodes"][arc["endNode"]]["stateName"]
+                if state_arc_leaves == current_state:
+                    arc_dict = {"expression": arc["outputText"],
+                                "next_state": state_arc_goes_to}
+                    current_state_data["arcs"].append(arc_dict)
+
+            for arc in self.fsm_json["fsmSelfArcs"]:
+                state_arc_is_on = self.fsm_json["fsmNodes"][arc["node"]]["stateName"]
+                if state_arc_is_on == current_state:
+                    arc_dict = {"expression": arc["outputText"],
+                                "next_state": state_arc_is_on}
+                    current_state_data["arcs"].append(arc_dict)
+                    
+            self.state_data.append(current_state_data)
+
+    def get_states_from_json(self, notation = "Q"):
+        self.state_names = []
+        for node in self.fsm_json["fsmNodes"]:
+            self.state_names.append(node["stateName"])
+        self.state_names = sorted(self.state_names)
+
+        num_state_bits = len(self.state_names[0])
+        self.state_bit_combos = [f"{i:0{num_state_bits}b}" for i in range(2 ** num_state_bits)]
+
+        # Make state bit names like Q1, Q0, Q1+, Q0+
+        self.state_bit_names = [notation + str(i) for i in range(num_state_bits)]
+        self.state_bit_names.reverse()
+        self.next_state_bit_names = [i + "+" for i in self.state_bit_names]
+    
+    def get_inputs_from_json(self):
+        if self.fsm_json["inputs"]:
+            self.input_names = self.fsm_json["inputs"].replace(" ", "").split(",")
+        else:
+            inputs = []
+            for arc in self.fsm_json["fsmArcs"] + self.fsm_json["fsmSelfArcs"]:
+                text = arc["outputText"]
+                # replace everything but alphanumeric and underscore with space
+                text = re.sub("[^0-9a-zA-Z_]+", " " , text)
+                arc_inputs = text.split(" ")
+                # Put non-empty, non-duplicate strings
+                inputs = inputs + [i for i in arc_inputs if i and i not in inputs]
+            # Remove strings that are made up of other strings, like "ab"
+            for i in inputs:
+                for j in inputs:
+                    if i + j in inputs:
+                        inputs.remove(i + j)
+            self.input_names = inputs
+            
+        num_input_bits = len(self.input_names)
+        self.input_combos = [f"{i:0{num_input_bits}b}" for i in range(2 ** num_input_bits)]
+
+    def get_outputs_from_json(self):
+    # Get Moore outputs
+        outputs = []
+        for node in self.fsm_json["fsmNodes"]:
+            text = node["outputText"]
+            # replace everything but alphanumeric and underscore with space
+            text = re.sub("[^0-9a-zA-Z_]+", " " , text)
+            node_outputs = text.split(" ")
+            # Put non-empty, non-duplicate strings that don't start with a number
+            outputs = outputs + [o for o in node_outputs if o and o not in outputs and not o[0].isdigit()]
+        self.output_names = outputs
+
+    def make_output_columns(self):
+
+        # First make all possible combinations of state with input
+        # (sorta like TT rows but without the outputs)
+        all_combos = itertools.product(self.state_bit_combos, self.input_combos)
+
+        next_state_columns = {}
+        
+
+        for combo in all_combos:
+            state_combo = combo[0]
+            input_combo = combo[1]
+
+            # Get the dictionary with the appropriate state
+            state_datum = [d for d in self.state_data if d["state"] == state_combo]
+
+            # If the state combo is used, figure out the appropriate next state
+            if state_datum:
+                state_datum = state_datum[0]
+                arcs = state_datum["arcs"]
+                for arc in arcs:
+                    expression = arc["expression"]
+                    if logic_eval(self.input_names, input_combo):
+
+
+                
+
+
+
+            
+                    
+                
